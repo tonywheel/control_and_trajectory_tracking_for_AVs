e62e7721164a2a8397083a40f0c9585dbc63c96e
tonywheel <asrueda95@gmail.com>
2022-01-07 20:38:31 +0000

    tuned PID

--- nd013-c6-control-starter/project/pid_controller/main.cpp
+++ nd013-c6-control-starter/project/pid_controller/main.cpp
@@ -236,9 +236,9 @@ int main ()
   
   
   PID pid_steer = PID();
-  pid_steer.Init(0.15, 0.05, 0.05, 1.2, -1.2);
+  pid_steer.Init(0.5, 0.0, 0.0, 1.2, -1.2);
   PID pid_throttle = PID();
-  pid_throttle.Init(0.4, 0.1, 0.1, 1, -1);
+  pid_throttle.Init(0.5, 0.0, 0.0, 1, -1);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
